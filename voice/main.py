4424b4257553ebd338dedc9259e9f05905892c6e
Evan Anderson <evan.k.anderson@gmail.com>
2019-03-31 10:51:53 -0700

    Fixed ordering and transaction start

--- voice/main.py
+++ voice/main.py
@@ -11,66 +11,182 @@ db = firestore.Client()
 settings = {}
 
 
-def extract_user(request_json):
-  request_json = request_json.get('originalDetectIntentRequest', {}).get('payload', {})
-  logging.info(f'User is: "{request_json.get("user")}"')
-  jwt = request_json.get('user', {}).get('idToken', '')
-  if not jwt:
-    return {'name': 'Anonymous', 'sub': 0}
-  auth_request = requests.Request()
-  info = id_token.verify_oauth2_token(jwt, auth_request)
-  if info['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
-    raise AssertionError('Wrong JWT issuer: %s', info['iss'])
-  return info
+def ensure_settings():
+    if not settings:
+        config = db.document('config/app').get()
+        settings.update(config.to_dict())
 
 
-def sale_items(request_json):
+def response(text: str, **kwargs):
+    resp = {'fulfillmentText': text}
+    resp.update(kwargs)
+    logging.info(f'Responding with: "{resp}"')
+    return resp
+
+
+def extract_user(request_json: dict):
+    request_json = request_json.get('originalDetectIntentRequest', {}).get(
+        'payload', {})
+    logging.info(f'User is: "{request_json.get("user")}"')
+    jwt = request_json.get('user', {}).get('idToken', '')
+    if not jwt:
+        return {'name': 'Anonymous', 'sub': 0}
+    auth_request = requests.Request()
+    info = id_token.verify_oauth2_token(jwt, auth_request)
+    if info['iss'] not in [
+            'accounts.google.com', 'https://accounts.google.com'
+    ]:
+        raise AssertionError('Wrong JWT issuer: %s', info['iss'])
+    return info
+
+
+def sale_items(request_json: dict):
     quote = request_json['queryResult'].get('fulfillmentText', 'Nothing')
-    return dict(fulfillmentText='I said "%s"' % quote)
+    return response('I said "%s"' % quote)
 
 
-def welcome(request_json):
+def welcome(request_json: dict):
     OPTS = [
         ' How may I serve you today?', ' It\'s a great day for a demo.',
         ' What can I get for you?'
     ]
-    return dict(fulfillmentText=random.choice(OPTS))
+    return response(random.choice(OPTS))
 
 
-def list_menu(request_json):
+def list_menu(request_json: dict):
     dishes = [x for x in model.AllDishes(db)]
     concat = ', '.join((x.name for x in dishes))
     count = len(dishes)
     user = extract_user(request_json)
     logging.info(f'User is {user}')
     OPTS = [
-      f'We have {count} items on the menu: {concat}',
-      f'{user["given_name"]}, I have {concat}'
+        f'We have {count} items on the menu: {concat}', f'We have {concat}'
     ]
-    return dict(fulfillmentText=random.choice(OPTS))
+    return response(random.choice(OPTS))
 
 
-def start_order(request_json):
-    # create a context for items to be added
-    return dict(fulfillmentText='Not implemented yet.')
+def start_order(request_json: dict):
+    ensure_settings()
+    # Request transaction to be set up.
+    return {
+        'payload': {
+            'google': {
+                'expectUserResponse': True,
+                'systemIntent': {
+                    'intent': 'actions.intent.TRANSACTION_REQUIREMENTS_CHECK',
+                    'data': {
+                        '@type':
+                        'type.googleapis.com/google.actions.v2.TransactionRequirementsCheckSpec',
+                        'orderOptions': {
+                            'requestDeliveryAddress': False
+                        },
+                        'paymentOptions': {
+                            'googleProvidedOptions': {
+                                'supportedCardNetworks':
+                                ['VISA', 'MASTERCARD'],
+                                'tokenizationParameters': {
+                                    'tokenizationType': 'PAYMENT_GATEWAY',
+                                    'parameters': {
+                                        'gateway':
+                                        'square',
+                                        'gatewayMerchantId':
+                                        settings.get('square_id', 'TODO'),
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    # followup = {
+    #     'name': 'actions.intent.TRANSACTION_REQUIREMENTS_CHECK',
+    #     'languageCode': 'en',
+    #     'parameters': {
+    #         'orderOptions': { 'requestDeliveryAddress': False},
+    #         'paymentOptions': {
+    #             'googleProvidedOptions': {
+    #                 'supportedCardNetworks': ['VISA', 'MASTERCARD'],
+    #                 'tokenizationParameters': {
+    #                     'tokenizationType': 'PAYMENT_GATEWAY',
+    #                     'parameters': {
+    #                         'gateway': 'square',
+    #                         'gatewayMerchantId': settings.get(
+    #                             'square_id', 'TODO'),
+    #                     }
+    #                 }
+    #             }
+    #         }
+    #     }
+    # }
+
+    # r = response(
+    #     'Not implemented yet.',
+    #     #followupEventInput=followup,
+    #     outputContexts=request_json.get('queryResult', {}).get(
+    #         'outputContexts', []))
+    # logging.info(f'Returning "{r}""')
+    # return r
 
 
-def add_item(request_json):
+def validate_payment(request_json: dict):
+    inputs = request_json.get('originalDetectIntentRequest', {}).get(
+        'payload', {}).get('inputs', [])
+    okay = False
+    logging.info(f'Looking in inputs: {inputs}')
+    for item in inputs:
+        args = item.get('arguments', [])
+        for arg in args:
+            if arg.get('name') == 'TRANSACTION_REQUIREMENTS_CHECK_RESULT':
+                okay = arg.get('extension', {}).get('resultType') == 'OK'
+    if not okay:
+        return response('Sorry, I couldn\'t get transaction information.')
+
+    user = extract_user(request_json)
+    _, doc = db.collection('orders').add({'user': user['sub'], 'items': []})
+
+    logging.info(doc.__dict__)
+
+    order_context = {
+        'name': f'{request_json["session"]}/contexts/order',
+        'lifespanCount': 5,
+        'parameters': {
+            'orderId': doc.id,
+        }
+    }
+    return response(
+        'Okay, let\'s start your order',
+        outputContexts=[order_context],
+        followupEventInput={
+            'name': 'menu',
+            'languageCode': 'en'
+        })
+
+
+def add_item(request_json: dict):
     user = extract_user(request_json)
     if not 'email' in 'user':
-      return dict(fulfillmentText='Please log in first')
-    return dict(fulfillmentText=f'Got it, {user["email"]}')
+        return response('Please log in first')
+    return response(f'Got it, {user["email"]}')
+
+
+def test(request_json: dict):
+    return response('Dynamic answer')
+
 
 HANDLERS = {
     'sale items': sale_items,
     'Default Welcome Intent': welcome,
     'ls': list_menu,
     'buy': start_order,
+    'start': validate_payment,
     'add': add_item,
+    'test': test,
 }
 
 
-def build_response(request_json):
+def build_response(request_json: dict):
     """Builds a dialogflow response to the given query.
 
     Arguments:
@@ -87,8 +203,7 @@ def build_response(request_json):
 
 def voice(request):
     data = request.get_json()
-    logging.info("Got message: %s",
-                    json.dumps(data, sort_keys=True))
+    logging.info("Got message: %s", json.dumps(data, sort_keys=True))
     response = build_response(data)
     return json.dumps(response)
 
